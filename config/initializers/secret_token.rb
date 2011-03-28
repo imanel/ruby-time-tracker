be93af149505dbe8377b1c0b440856eb619ed718
Bernard Potocki <bernard.potocki@imanel.org>
2011-03-28 19:15:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyTimeTracker::Application.config.secret_token = '534627b837d5308198249647bae31c72f6ee52675fb53453304563ce29fc554cd9efc9ea4ca41ab81f2dfb6cac941f9b0f3809fcab1e35b91dc7831be080ffb2'
